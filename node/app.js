8cc99ad5af0d0817af2b86b4d69289cefb43999e
RyougiOrange <tianlonghuangwu@outlook.com>
2020-08-21 23:27:28 +0800

    push my demo code

--- node/app.js
+++ node/app.js
@@ -1,19 +1,17 @@
-const { say } = require('../pkg/ssvm_nodejs_starter_lib.js');
+const express = require('express');
+const { getpi } = require('../pkg/ssvm_nodejs_starter_lib.js');
 
-const http = require('http');
-const url = require('url');
-const hostname = '0.0.0.0';
+const app = express();
 const port = 3000;
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: false }));
 
-const server = http.createServer((req, res) => {
-  const queryObject = url.parse(req.url,true).query;
-  if (!queryObject['name']) {
-    res.end(`Please use command curl http://${hostname}:${port}/?name=MyName \n`);
-  } else {
-    res.end(say(queryObject['name']) + '\n');
-  }
-});
+app.get('/', (req, res) => res.redirect("/index.html"));
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.post('/solve', function (req, res) {
+  console.log(req.body.inputnum)
+  let pinum = parseInt(req.body.inputnum);
+  res.send(getpi([pinum]))
+})
+
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,55 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn say(s: &str) -> String {
-  println!("The Rust function say() received {}", s);
-  let r = String::from("hello ");
-  return r + s;
+pub fn getpi(params: i32) -> String {
+  let mut ps: i32 = params;
+  let mut output: String = "".into();
+  let a = 10000;
+  let mut b = 0;
+  let mut c = 2800;
+  let mut d = 0;
+  let mut e = 0;
+  let mut f = [0; 2801];
+  let mut g = 0;
+  let mut i: i32;
+  for i in 0..c {
+    f[i] = a / 5;
+  }
+  while c != 0 {
+    if ps <= 0 {
+      break;
+    }
+    d = 0;
+    g = c * 2;
+    b = c;
+    loop {
+      d = d + f[b] * a;
+      g -= 1;
+      f[b] = d % g;
+      d = d / g;
+      g -= 1;
+      b -= 1;
+      if b == 0 {
+        break;
+      } else {
+        d = d * b;
+        continue;
+      }
+    }
+    c = c - 14;
+    ps -= 4;
+    let num = e + d / a;
+    if output == "" {
+      output += "3.141";
+    } else {
+      if num < 1000 {
+        output += "0";
+      } else if num < 100 {
+        output += "00";
+      }
+      output += &num.to_string();
+    }
+    e = d % a;
+  }
+  return output;
 }
